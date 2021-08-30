ca587ef1bf4b1276ca1f9fa69270c28ffb0fe867
Carlo Maria Vendelbak Mercuri <carl492h@zbc.dk>
2021-08-30 09:56:50 +0200

    Add project files.

---
+++ ZBC_OOP_Square/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ZBC_OOP_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave A
+
+            Console.WriteLine("----------------- OPGAVE A ------------------");
+
+            Square sq1 = new Square(5);
+            Console.WriteLine($"Square 1 - Side Lenght: {sq1.SideLenght} Area: {sq1.GetArea()}, Perimeter: {sq1.GetPerimeter()}");
+
+            Square sq2 = new Square(2);
+            Console.WriteLine($"Square 2 - Side Lenght: {sq2.SideLenght} Area: {sq2.GetArea()}, Perimeter: {sq2.GetPerimeter()}");
+
+            Square sq3 = new Square(19);
+            Console.WriteLine($"Square 3 - Side Lenght: {sq3.SideLenght} Area: {sq3.GetArea()}, Perimeter: {sq3.GetPerimeter()}");
+
+            Square sq4 = new Square(28);
+            Console.WriteLine($"Square 4 - Side Lenght: {sq4.SideLenght} Area: {sq4.GetArea()}, Perimeter: {sq4.GetPerimeter()}");
+
+            // Opgave B
+
+            // Changing side lenghts of each square
+            sq1.SideLenght = 15;
+            sq2.SideLenght = 5;
+            sq3.SideLenght = 33;
+            sq4.SideLenght = 55;
+
+            Console.WriteLine("");
+            Console.WriteLine("----------------  OPGAVE B ----------------");
+            Console.WriteLine("");
+            Console.WriteLine("Changed the side lenght values for the squares!");
+            Console.WriteLine("");
+            Console.WriteLine("New values:");
+            Console.WriteLine("");
+
+            Console.WriteLine($"Square 1 - Side Lenght: {sq1.SideLenght} Area: {sq1.GetArea()}, Perimeter: {sq1.GetPerimeter()}");
+
+
+            Console.WriteLine($"Square 2 - Side Lenght: {sq2.SideLenght} Area: {sq2.GetArea()}, Perimeter: {sq2.GetPerimeter()}");
+
+            Console.WriteLine($"Square 3 - Side Lenght: {sq3.SideLenght} Area: {sq3.GetArea()}, Perimeter: {sq3.GetPerimeter()}");
+
+            Console.WriteLine($"Square 4 - Side Lenght: {sq4.SideLenght} Area: {sq4.GetArea()}, Perimeter: {sq4.GetPerimeter()}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ZBC_OOP_Square/Square.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBC_OOP_Square
+{
+    public class Square
+    {
+        private float side { get; set; }
+
+        public float SideLenght
+        {
+            get { return side; }
+            set { side = value; }
+        }
+
+        public Square(float _sideLenght)
+        {
+            side = _sideLenght;
+        }
+
+        public Square()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns the lenght of the perimeter
+        /// </summary>
+        /// <returns></returns>
+        public float GetPerimeter()
+        {
+            // 4 times the side lenght
+            return side * 4;
+        }
+
+        /// <summary>
+        /// Returns the area of the square
+        /// </summary>
+        /// <returns></returns>
+        public float GetArea()
+        {
+            // Base * height (same in a square)
+            return side * side;
+        }
+    }
+}
